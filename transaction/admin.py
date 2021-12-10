18628900668c497865391acac15c6675e24bc8f2
armin2080 <arminmaddah.a@gmail.com>
2021-12-10 23:16:06 +0330

    .

--- transaction/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.contrib.admin.sites import site
-
-from factor.models import FactorProducts, Factors
-from transaction.models import Transactions
-
-
-# Register your models here.
-admin.site.register(Transactions)
--- transaction/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('',views.ListTrans.as_view,name="list_trans"),
-    
-
-]
